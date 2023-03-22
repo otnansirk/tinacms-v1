b1bde166aadcb345c8dcb6e3029394ac526b7164
k15 <krisnantobiyuh@gmail.com>
2023-03-22 12:56:51 +0700

    feat : update bg marketing

--- components/marketing/Marketing.js
+++ components/marketing/Marketing.js
@@ -1,10 +1,11 @@
 function PageSectionsMarketing(props) {
     const {
         title,
+        background,
         __typename
     } = props;
 
-    return <div id={__typename}>
+    return <div id={__typename} style={{background, padding: '10px'}}>
         <h4>{title}</h4>
     </div>
 }
