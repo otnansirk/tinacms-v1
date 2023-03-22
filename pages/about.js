6a4ec202c39123cbeecb3843598a6c1381f4eb32
k15 <krisnantobiyuh@gmail.com>
2023-03-22 11:15:11 +0700

    feat : update index and about page

--- pages/about.js
+++ pages/about.js
@@ -3,25 +3,28 @@ import { useTina } from "tinacms/dist/react";
 import { client } from "../.tina/__generated__/client";
 import DynamicComponent from "../components/DynamicComponent.js";
 
-function HomePage(props) {
-const { data, loading } = useTina({
-    query: props.query,
-    variables: props.variables,
-    data: props.data,
-})
+
+function AboutPage(props) {
+    const { data, loading } = useTina({
+        query     : props.query,
+        variables : props.variables,
+        data      : props.data,
+    })
+
 
     return <div>
         <h1>{loading ? 'Loading...' : data.page.title}</h1>
         <DynamicComponent sections={data.page?.sections}/>
     </div>
+
 }
 
-export default HomePage
+export default AboutPage
 
 
 export const getStaticProps = async () => {
     const { data, query, variables } = await client.queries.page({
-        relativePath: "home.md",
+        relativePath: "about.md",
     });
 
     return {
