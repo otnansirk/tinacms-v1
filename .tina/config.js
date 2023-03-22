16c25b3fd673d6b7ddee0bb673238f1b78417296
k15 <krisnantobiyuh@gmail.com>
2023-03-22 11:14:36 +0700

    feat : update tina config.js

--- .tina/config.js
+++ .tina/config.js
@@ -1,25 +1,95 @@
 import { defineSchema, defineConfig } from 'tinacms'
 
 const schema = defineSchema({
-    collections: [{
-        label: "Post",
-        name: "post",
-        path: "posts",
-        ui: {
-            router: ({ document }) => {
-                if (document._sys.filename === 'hello-word') {
-                    return `/`
-                }
-                return undefined
+    collections: [
+        {
+            label: "Pages",
+            name: "page",
+            path: "contents/page",
+            format: "md",
+            ui: {
+                router: ({ document }) => {
+                    if (document._sys.filename === 'home') {
+                        return `/`
+                    }
+                    if (document._sys.filename === 'about') {
+                        return `/about`
+                    }
+                    return undefined
+                },
             },
-        },
-        fields: [{
-            label: "Title",
-            name: "title",
-            type: "string"
-        }]
-    }]
+            fields: [
+                {
+                    label: "Title",
+                    name: "title",
+                    type: "string"
+                },
+                {
+                    label: "Description",
+                    name: "description",
+                    type: "string"
+                },
+                {
+                    label: 'Contact Info',
+                    name: 'contact',
+                    type: "object",
+                    fields: [
+                        {
+                            label: 'Email',
+                            name: 'email',
+                            type: 'string',
+                        }
+                    ],
+                },
+                {
+                    label: 'Sections',
+                    name: 'sections',
+                    type: "object",
+                    list: true,
+                    templates: [
+                        {
+                            name: 'benefit',
+                            label: 'Benefit',
+                            fields: [
+                                {
+                                    label: "Title",
+                                    name: "title",
+                                    type: "string"
+                                }
+                            ],
+                        },
+                        {
+                            name: 'marketing',
+                            label: 'Marketing',
+                            fields: [
+                                {
+                                    label: "Title",
+                                    name: "title",
+                                    type: "string"
+                                }
+                            ],
+                        },
+                    ]
+                }
+            ]
+        }
+    ]
 })
+
+export const HeaderBlock = {
+    label: 'Header',
+    key: 'header-block',
+    defaultItem: {
+        content: '',
+    },
+    fields: [
+        {
+            label: 'Content',
+            name: 'content',
+            type: 'string'
+        }
+    ]
+}
 
 const config = defineConfig({
     build: {
@@ -28,5 +98,6 @@ const config = defineConfig({
     },
     schema
 })
+
 
 export default config
