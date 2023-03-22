3e1390d264d6ca49c53e608d6fefe3d7fa126a33
k15 <krisnantobiyuh@gmail.com>
2023-03-22 21:58:58 +0700

    feat : add env to  next.config and update package.json

--- next.config.js
+++ next.config.js
@@ -7,4 +7,9 @@ module.exports = {
             },
         ]
     },
+    env: {
+        NEXT_TINA_BRANCH : process.env.NEXT_TINA_BRANCH,
+        NEXT_TINA_TOKEN : process.env.NEXT_TINA_TOKEN,
+        NEXT_TINA_ID : process.env.NEXT_TINA_ID
+    }
 }
