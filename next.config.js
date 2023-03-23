fd98207c3c7c5c4bd339d0eef868a90e01d1c2fb
k15 <krisnantobiyuh@gmail.com>
2023-03-23 21:38:18 +0700

    ref : update to typescript

--- next.config.js
+++ next.config.js
@@ -8,9 +8,17 @@ module.exports = {
         ]
     },
     env: {
-        NEXT_TINA_BRANCH : process.env.NEXT_TINA_BRANCH,
-        NEXT_TINA_TOKEN : process.env.NEXT_TINA_TOKEN,
-        NEXT_TINA_ID : process.env.NEXT_TINA_ID
+        NEXT_TINA_BRANCH: process.env.NEXT_TINA_BRANCH,
+        NEXT_TINA_TOKEN: process.env.NEXT_TINA_TOKEN,
+        NEXT_TINA_ID: process.env.NEXT_TINA_ID,
+        NEXT_MONGO_URI: process.env.NEXT_MONGO_URI,
+
+        GITHUB_OWNER: process.env.NEXT_GITHUB_OWNER,
+        GITHUB_REPO: process.env.NEXT_GITHUB_REPO,
+        GITHUB_BRANCH: process.env.NEXT_GITHUB_BRANCH,
+        GITHUB_PERSONAL_ACCESS_TOKEN: process.env.NEXT_GITHUB_PERSONAL_ACCESS_TOKEN,
+        TINA_PUBLIC_IS_LOCAL: process.env.NEXT_TINA_PUBLIC_IS_LOCAL,
+        PUBLIC_TINA_CLIENT_ID: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
     },
     "jsx": "react-jsx"
 }
---
+++ pages/[slugify].tsx
@@ -0,0 +1,47 @@
+import { useTina } from "tinacms/dist/react";
+// import { client } from "../.tina/__generated__/client";
+import Blocks from "../components/blocks-renderer";
+import { dbConnection } from "../lib/databaseConnection";
+
+function RootPage(props) {
+    const { data } = useTina({
+        query: props.query,
+        variables: props.variables,
+        data: props.data,
+    })
+
+    return <div>
+        <h1>{data?.page?.title}</h1>
+        <Blocks sections={data.page?.sections} />
+    </div>
+}
+
+export default RootPage
+
+
+export const getStaticProps = async ({ params }) => {
+    const { data, query, variables } = await dbConnection.queries.page({
+        relativePath: `${params.slugify}.json`,
+    });
+
+    return {
+        props: {
+            data,
+            query,
+            variables,
+        },
+    }
+}
+
+export const getStaticPaths = async () => {
+    const pagesListData = await dbConnection.queries.pageConnection();
+
+    return {
+        paths: pagesListData.data.pageConnection.edges.map((page) => {
+            return {
+                params: { slugify: page.node._sys.filename },
+            }
+        }),
+        fallback: false,
+    };
+};
