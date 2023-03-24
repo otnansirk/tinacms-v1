b285aefe4b32ea4b94fa2c77f35001aa82388e73
k15 <krisnantobiyuh@gmail.com>
2023-03-24 21:29:13 +0700

    feat : update marketing component

--- components/marketing/Marketing.tsx
+++ components/marketing/Marketing.tsx
@@ -2,11 +2,16 @@ function PageSectionsMarketing(props) {
     const {
         title,
         background,
+        image,
         __typename
     } = props;
 
     return <div id={__typename} style={{background, padding: '10px'}}>
         <h4>{title}</h4>
+        {
+            image &&
+                <img src={image} alt="image" width="50%"/>
+        }
     </div>
 }
 
